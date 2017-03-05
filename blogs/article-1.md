580051cbade645f50de5656935536eacb6a0d8b4
luckyhanye <404133902@qq.com>
2017-03-05 10:08:04 +0800

    blogs

--- blogs/article-1.md
+++ blogs/article-1.md
@@ -5,3 +5,11 @@ title: 第一篇文章
 ## 第一篇文章
 
 ### 标题一
+
+```
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
+```
--- src/component/post.js
+++ src/component/post.js
@@ -1,18 +1,36 @@
 import React from "react"
+import axios from "axios"
+import marked from "marked"      //转译markeddown  语法
+import hljs from "highlight.js"
+
+
+
+marked.setOptions({
+  highlight: function (code) {
+    return hljs.highlightAuto(code).value
+  }
+})
+
 
 class Post extends React.Component{
   constructor(){
     super();
     this.state={
-
+      data:''
     }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/luckyhanye/myblog/master/blogs/${this.props.params.title}.md`)
+      .then(res=>this.setState({data:res.data}))
   }
   render(){
     console.log("post======",this.props);
     return(
       <div>
         文章内容
-        {this.props.params.title}
+        {this.state.data.length==0?'加载中……':
+        <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}></div>
+        }
       </div>
     )
   }
